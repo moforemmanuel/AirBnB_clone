079b6db13b6f227a04d8e8db0c2cf1ab8f4036a8
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-08-03 16:29:29 +0100

    task 3 - first basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
     # __dict__ = {}
 
     def __init__(self):
-        self.id = uuid4()
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
@@ -38,9 +38,14 @@ class BaseModel:
 
 
 if __name__ == "__main__":
-    b = BaseModel()
-    # print(b.id)
-    # print(b.created_at)
-    b.name = 'yo'
-    print(b.to_dict())
-    print(type(b.id))
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
