f9e39888e0affd98d84c56504476f4da287cf306
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-08-04 02:34:10 +0100

    4 - create basemodel from dict

--- models/base_model.py
+++ models/base_model.py
@@ -16,10 +16,22 @@ class BaseModel:
     """
     # __dict__ = {}
 
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            # attrs = {}
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    continue
+                if k in ('created_at', 'updated_at'):
+                    v = datetime.strptime(v, DATE_FORMAT)
+                    # setattr(self, k, v)
+
+                setattr(self, k, v)
+
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -38,14 +50,38 @@ class BaseModel:
 
 
 if __name__ == "__main__":
+    # my_model = BaseModel()
+    # my_model.name = "My First Model"
+    # my_model.my_number = 89
+    # print(my_model)
+    # my_model.save()
+    # print(my_model)
+    # my_model_json = my_model.to_dict()
+    # print(my_model_json)
+    # print("JSON of my_model:")
+    # for key in my_model_json.keys():
+    #     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), \
+    #     my_model_json[key]))
+
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-    my_model.save()
-    print(my_model)
+    print(type(my_model.created_at), my_model.created_at)
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
+                                       my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at), my_new_model.created_at)
+
+    print("--")
+    print(my_model is my_new_model)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,6 +9,14 @@ from models.base_model import BaseModel
 from uuid import UUID
 from datetime import datetime
 
+INSTANCE_DICT = {'id': '859e55ea-63ed-4087-bd02-514dff7df00b',
+                 'created_at': '2022-08-03T20:45:21.158641',
+                 'updated_at': '2022-08-03T20:45:21.158657',
+                 'name': 'My_First_Model', 'my_number': 89,
+                 '__class__': 'BaseModel'}
+
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class MyTestCase(unittest.TestCase):
     """
@@ -20,8 +28,11 @@ class MyTestCase(unittest.TestCase):
         :return: test
         """
         base = BaseModel()
-        self.assertIsInstance(base.id, UUID)  # add assertion here
+        self.assertIsInstance(base.id, str)  # add assertion here
         self.assertIsInstance(base.created_at, datetime)
+        base_2 = BaseModel(**INSTANCE_DICT)
+        self.assertIsInstance(base_2, BaseModel)
+        self.assertEqual(base_2.created_at, datetime.strptime(INSTANCE_DICT['created_at'], DATE_FORMAT))
 
     def test_of_to_dict(self):
         """
@@ -38,7 +49,6 @@ class MyTestCase(unittest.TestCase):
         base = BaseModel()
         base.save()
         self.assertIsInstance(base.updated_at, datetime)
-
 
 
 
