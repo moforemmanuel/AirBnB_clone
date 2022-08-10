68e36754668bea33ce3cc872cc6eee8c1e3be259
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-08-10 04:04:19 +0100

    asserted Tests and updated <class>.Updated()

--- console.py
+++ console.py
@@ -283,16 +283,17 @@ class HBNBCommand(cmd.Cmd):
                 commands[args[1]](args[0] + ' ' + args[2])
             elif args[1] == "update":
                 params = re.match(r"\"(.+?)\", (.+)", args[2])
-                if params.groups()[1][0] == '{':
-                    dic_p = eval(params.groups()[1])
-                    for k, v in dic_p.items():
-                        commands[args[1]](args[0] + " " + params.groups()[0] +
-                                          " " + k + " " + str(v))
-                else:
-                    rest = params.groups()[1].split(", ")
-                    commands[args[1]](args[0] + " " +
-                                      params.groups()[0] + " " +
-                                      rest[0] + " " + rest[1])
+                if params:
+                    if params.groups()[1][0] == '{':
+                        dic_p = eval(params.groups()[1])
+                        for k, v in dic_p.items():
+                            commands[args[1]](args[0] + " " + params.groups()[0] +
+                                              " " + k + " " + str(v))
+                    else:
+                        rest = params.groups()[1].split(", ")
+                        commands[args[1]](args[0] + " " +
+                                          params.groups()[0] + " " +
+                                          rest[0] + " " + rest[1])
 
         if not args or len(args) < 2 or args[0] not in names \
                 or args[1] not in commands.keys():
