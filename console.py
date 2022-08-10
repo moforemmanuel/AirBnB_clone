71de5e306b73d7f6e688e5998f77c64d3f2b7bfa
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-08-10 03:28:20 +0100

    implemented advanced tasks

--- console.py
+++ console.py
@@ -239,6 +239,66 @@ class HBNBCommand(cmd.Cmd):
                 setattr(instance, attrib, adjusted_value)
                 instance.save()
 
+    def my_count(self, class_n):
+        """
+        Method counts instances of a certain class
+        """
+        count_instance = 0
+        for instance_object in models.storage.all().values():
+            if instance_object.__class__.__name__ == class_n:
+                count_instance += 1
+        print(count_instance)
+
+    def default(self, line):
+        """Method to take care of following commands:
+        <class name>.all()
+        <class name>.count()
+        <class name>.show(<id>)
+        <class name>.destroy(<id>)
+        <class name>.update(<id>, <attribute name>, <attribute value>)
+        <class name>.update(<id>, <dictionary representation)
+        Description:
+            Creates a list representations of functional models
+            Then use the functional methods to implement user
+            commands, by validating all the input commands
+        """
+        names = ["BaseModel", "User", "State", "City", "Amenity",
+                 "Place", "Review"]
+
+        commands = {"all": self.do_all,
+                    "count": self.my_count,
+                    "show": self.do_show,
+                    "destroy": self.do_destroy,
+                    "update": self.do_update}
+
+        args = re.match(r"^(\w+)\.(\w+)\((.*)\)", line)
+        if args:
+            args = args.groups()
+            # print(args)
+
+            if args[1] in ["all", "count"]:
+                # print('args in it')
+                commands[args[1]](args[0])
+            elif args[1] in ["show", "destroy"]:
+                commands[args[1]](args[0] + ' ' + args[2])
+            elif args[1] == "update":
+                params = re.match(r"\"(.+?)\", (.+)", args[2])
+                if params.groups()[1][0] == '{':
+                    dic_p = eval(params.groups()[1])
+                    for k, v in dic_p.items():
+                        commands[args[1]](args[0] + " " + params.groups()[0] +
+                                          " " + k + " " + str(v))
+                else:
+                    rest = params.groups()[1].split(", ")
+                    commands[args[1]](args[0] + " " + params.groups()[0] + " " +
+                                      rest[0] + " " + rest[1])
+
+        if not args or len(args) < 2 or args[0] not in names \
+                or args[1] not in commands.keys():
+            super().default(line)
+        return
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
